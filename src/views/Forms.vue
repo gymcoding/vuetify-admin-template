607139a08890742661ebfdc8aefd486c5f4d0723
GymCoding <bruce.lean17@gmail.com>
2021-05-13 10:50:13 +0900

    Forms 유효성 추가
    Forms 유효성 추가

--- src/views/Forms.vue
+++ src/views/Forms.vue
@@ -5,66 +5,162 @@
         Stacked Forms
       </v-card-title>
       <v-card-text>
-        <v-form
-          ref="form"
-          @submit.prevent="submit"
+        <validation-observer
+          ref="observer"
+          v-slot="{ invalid }"
         >
-          <v-text-field
-            v-model="name"
-            :counter="10"
-            label="Name"
-          />
-
-          <v-text-field
-            v-model="phoneNumber"
-            :counter="7"
-            label="Phone Number"
-          />
-
-          <v-text-field
-            v-model="email"
-            label="E-mail"
-          />
-
-          <v-select
-            v-model="select"
-            :items="items"
-            label="Select"
-            data-vv-name="select"
-          />
-
-          <v-checkbox
-            v-model="checkbox"
-            label="Option"
-            type="checkbox"
-          />
-
-          <v-btn
-            class="mr-4"
-            type="submit"
+          <v-form
+            ref="form"
+            @submit.prevent="submit"
           >
-            submit
-          </v-btn>
-          <v-btn @click="clear">
-            clear
-          </v-btn>
-        </v-form>
+            <validation-provider
+              v-slot="{ errors }"
+              name="Name"
+              :rules="{
+                required: true,
+                max: 10
+              }"
+            >
+              <v-text-field
+                v-model="name"
+                :counter="10"
+                label="Name"
+                :error-messages="errors"
+              />
+            </validation-provider>
+
+            <validation-provider
+              v-slot="{ errors }"
+              name="Phone Number"
+              :rules="{
+                required: true,
+                numeric: true,
+                digits: 11,
+              }"
+            >
+              <v-text-field
+                v-model="phoneNumber"
+                :counter="11"
+                label="Phone Number"
+                :error-messages="errors"
+              />
+            </validation-provider>
+
+            <validation-provider
+              v-slot="{ errors }"
+              name="E-mail"
+              :rules="{
+                required: true,
+                email: true
+              }"
+            >
+              <v-text-field
+                v-model="email"
+                label="E-mail"
+                :error-messages="errors"
+              />
+            </validation-provider>
+            <validation-provider
+              v-slot="{ errors }"
+              name="select"
+              rules="required"
+            >
+              <v-select
+                v-model="select"
+                :items="items"
+                label="Select"
+                :error-messages="errors"
+              />
+            </validation-provider>
+
+            <validation-provider
+              v-slot="{ errors }"
+              name="checkbox"
+              :rules="{
+                required: true
+              }"
+            >
+              <v-checkbox
+                v-model="checkbox"
+                value="1"
+                label="Option"
+                type="checkbox"
+                required
+                :error-messages="errors"
+              />
+            </validation-provider>
+
+            <v-btn
+              class="mr-4"
+              type="submit"
+              color="primary"
+              :disabled="invalid"
+            >
+              submit
+            </v-btn>
+            <v-btn @click="clear">
+              clear
+            </v-btn>
+          </v-form>
+        </validation-observer>
       </v-card-text>
     </v-card>
   </v-container>
 </template>
 <script>
+import {
+  extend,
+  ValidationObserver,
+  ValidationProvider,
+} from 'vee-validate'
+import {
+  email,
+  max,
+  min,
+  numeric,
+  required,
+  digits,
+} from 'vee-validate/dist/rules'
+
+extend('digits', {
+  ...digits,
+  message: '{_field_}는 {length} 자리 여야합니다. ({_value_})'
+})
+extend('email', {
+  ...email,
+  message: '잘못 입력된 이메일 주소입니다.',
+})
+extend('max', {
+  ...max,
+  message: '{_field_} 필드는 {length}자를 초과 할 수 없습니다.'
+})
+extend('min', {
+  ...min,
+  message: '{_field_} 필드는 {length}자 미만 이어야 합니다.'
+})
+extend('numeric', {
+  ...numeric,
+  message: '{_field_} 필드는 숫자로만 구성되어야합니다.'
+})
+extend('required', {
+  ...required,
+  message: '{_field_} 필드는 필수값 입니다.',
+})
+
 export default {
+  components: {
+    ValidationObserver,
+    ValidationProvider,
+  },
   data: () => ({
     name: '',
     phoneNumber: '',
     email: '',
     select: null,
     items: [
-      'Item 1',
-      'Item 2',
-      'Item 3',
-      'Item 4',
+      { text: '아이템1', value: 1 },
+      { text: '아이템2', value: 2 },
+      { text: '아이템3', value: 3 },
     ],
     checkbox: null,
   }),
